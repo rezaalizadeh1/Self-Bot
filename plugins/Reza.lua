1851c0f8c040e841c43a802606aaa5f3582a0c97
rezaalizadeh1 <amir.hatefi135@gmail.com>
2016-09-22 23:07:36 +0330

    Update and rename solid.lua to Reza.lua

--- plugins/Reza.lua
+++ plugins/Reza.lua
@@ -2,9 +2,9 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'بلی؟'
+local text = 'بله؟'
 --در اینجا میتونین جواب رو تعیین کنید
-if matches[1] == 'سلید' or 'solid' or 'سعید' or 'saeid' or 'saeed' then
+if matches[1] == 'reza' or 'r.h.z' or 'رضا' or 'رضااا' or'رضاا' then
     if not is_sudo(msg) then
 --در این قسمت میتونید اسم خودتون رو بزارید
 --یادتون باشه وقتی اسم خودتون رو گذاشتید پترن هارو هم تغییر بدید
@@ -14,11 +14,11 @@ end
 end
 return {
 patterns = {
-    "^سلید$",
-    "^سعید$",
-"^([Ss]aeid)$",
-"^([Ss]aeed)$",
-"^([Ss]olid)$"
+    "^عشقم$",
+    "^رضا$",
+"^([Rr]eza)$",
+"^([Rr]eza)$",
+"^([Rr].h.z)$"
 },
 run = run
 }
