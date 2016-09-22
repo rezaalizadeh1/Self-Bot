2c0a54566029b52d2e8605b984ee986d2e3edd1b
rezaalizadeh1 <amir.hatefi135@gmail.com>
2016-09-22 20:56:16 +0330

    Update solid.lua

--- plugins/solid.lua
+++ plugins/solid.lua
@@ -2,9 +2,9 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'بلی؟'
+local text = 'جونم'
 --در اینجا میتونین جواب رو تعیین کنید
-if matches[1] == 'سلید' or 'solid' or 'سعید' or 'saeid' or 'saeed' then
+if matches[1] == 'رضا' or 'reza' or 'رضاا' or'raz  'or 'a' then
     if not is_sudo(msg) then
 --در این قسمت میتونید اسم خودتون رو بزارید
 --یادتون باشه وقتی اسم خودتون رو گذاشتید پترن هارو هم تغییر بدید
@@ -14,11 +14,11 @@ end
 end
 return {
 patterns = {
-    "^سلید$",
-    "^سعید$",
-"^([Ss]aeid)$",
-"^([Ss]aeed)$",
-"^([Ss]olid)$"
+    "^عشقم$",
+    "^رضا$",
+"^([Re]eza)$",
+"^([Rr]hz)$",
+"^([Rr].h.z)$"
 },
 run = run
 }
